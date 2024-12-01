52befdbab4a504c9065f6625ed3ddae184cfa3e6
smatsuodev <git@smatsuo.dev>
2024-12-01 15:01:06 +0900

    add nvim-surround

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,4 +14,12 @@ return {
       vim.g.haskell_tools = {}
     end,
   },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {}
+    end,
+  },
 }
