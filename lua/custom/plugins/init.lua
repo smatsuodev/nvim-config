78439f2858b40085fc10e53b74e52527221b5ab1
smatsuodev <git@smatsuo.dev>
2024-12-01 12:21:05 +0900

    haskell-tools.nvimを導入

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'mrcjkb/haskell-tools.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    init = function()
+      vim.g.haskell_tools = {}
+    end,
+  },
+}
